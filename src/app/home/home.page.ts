c9a5b67a75e20de57d31ff9b636817843eb1a633
soiqualang <soiqualang.chentreu@gmail.com>
2023-10-30 12:30:34 +0700

    t1

---
+++ src/app/home/home.page.ts
@@ -0,0 +1,46 @@
+import { Component } from '@angular/core';
+import * as mapboxgl from 'mapbox-gl';
+import { environment } from 'src/environments/environment';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+})
+export class HomePage {
+	private mapbox: mapboxgl.Map;
+
+	constructor() {
+		mapboxgl.accessToken = environment.mapbox.accessToken;
+	}
+
+	ngOnInit() {
+		const mediaDarkMode = window.matchMedia('(prefers-color-scheme: dark)');
+		mediaDarkMode.addListener(this.switchMode);
+
+		this.initMap(mediaDarkMode.matches);
+	}
+
+	private initMap(isDarkModeEnebled: boolean) {
+		this.mapbox = new mapboxgl.Map({
+			container: 'mapbox',
+			style: `mapbox://styles/mapbox/${
+				isDarkModeEnebled ? 'dark' : 'light'
+			}-v10`,
+			zoom: 12,
+			center: [106.6889938,10.7789261],
+		});
+
+		this.mapbox.on('load', (event) => {
+			this.mapbox.resize();
+			window['map'] = this.mapbox;
+		});
+	}
+
+	switchMode(systemInitiatedDark) {
+		const mode = systemInitiatedDark.matches ? 'dark' : 'light';
+
+		document.body.setAttribute('data-theme', mode);
+		window['map'].setStyle(`mapbox://styles/mapbox/${mode}-v10`);
+	}
+}
